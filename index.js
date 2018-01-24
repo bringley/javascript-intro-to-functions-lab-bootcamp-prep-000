ed5bd5c8bbc12239e7b90f843ce489fd841cf6a6
Brian Ringley <brian.ringley@wework.com>
2018-01-24 22:29:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+//receive string argument and return all caps
 function shout(string){
   return string.toUpperCase()
 }
 
+//receive string argument and return all lowercase
 function whisper(string){
   return string.toLowerCase()
 }
+
+//
